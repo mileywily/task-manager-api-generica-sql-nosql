1de2e2831221fab6ec2d14045ee1758db1dbfd59
mileywily <mileida.blanco.y@gmail.com>
2026-03-09 17:26:04 -0300

    feat(app): register TasksModule in root AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { TasksModule } from './task/tasks.module'; // Importante
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule, 
+    TasksModule, // Asegúrate de que esta línea exista
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
